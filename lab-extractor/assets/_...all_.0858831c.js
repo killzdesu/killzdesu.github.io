ac9a4964f1755a9236ae5e6e506a0bd5e8399e70
killzdesu <killzdesu@gmail.com>
2022-06-19 17:35:07 +0700

    add lab-extractor

---
+++ lab-extractor/assets/_...all_.0858831c.js
@@ -0,0 +1,1 @@
+import{_ as r,b as o}from"./index.79e5bffa.js";import{o as t,a as _}from"./vendor.e1ccdabe.js";const e={};function c(n,a){return t(),_("div",null," Not found ")}typeof o=="function"&&o(e);var s=r(e,[["render",c]]);export{s as default};
